e0d63e8f9588b5401796807272ab48cf65e0386c
yang-zhao <zhaoyang_100@hotmail.com>
2014-06-02 17:01:05 +0900

    Update java_pack.rb

--- lib/java_pack.rb
+++ lib/java_pack.rb
@@ -38,25 +38,9 @@ class JavaPack
   def bash_script
     <<-BASH
 #!/usr/bin/env bash
-export JAVA_HOME="#{global.jdk_dir}"
-export PATH="#{global.jdk_dir}/bin:$PATH"
-
-for file in `find /tmp/ -type d`; do
-  echo "$file"
-done
-
-echo JAVA_HOME 
-echo PATH
-
-MEMORY_DIGIT=`echo $MEMORY_LIMIT |sed 's/[m|M]//g'`
-xmx=`expr $MEMORY_DIGIT / 2`
-max_mem=800
-if [ $xmx -gt $max_mem ];then
-  xmx=$max_mem
-fi
-# -Xmx${xmx}M
-# export JAVA_OPTS=${JAVA_OPTS:-"-Xms1024m -Xmx1024m -Xmn512m -XX:PermSize=32m -XX:MaxPermSize=64m -XX:SurvivorRatio=4 -verbose:gc -Xloggc:/home/vcap/logs/gc.log -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -Djava.awt.headless=true #{java_opts.map{ |k, v| "#{k}#{v}" }.join(' ')}"}
-export JAVA_OPTS=${JAVA_OPTS:-"-Xmx${xmx}M #{java_opts.map{ |k, v| "#{k}#{v}" }.join(' ')}"}
+export JAVA_HOME="$HOME/.jdk"
+export PATH="$HOME/.jdk/bin:$PATH"
+export JAVA_OPTS=${JAVA_OPTS:-"#{java_opts.map{ |k, v| "#{k}#{v}" }.join(' ')}"}
 export LANG="${LANG:-en_US.UTF-8}"
 
 if [ -n "$VCAP_DEBUG_MODE" ]; then
