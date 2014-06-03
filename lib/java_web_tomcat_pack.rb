f77715ab6dc755f680033976db5af6bfd7285ade
yang-zhao <zhaoyang_100@hotmail.com>
2014-06-03 11:41:44 +0900

    Update java_web_tomcat_pack.rb

--- lib/java_web_tomcat_pack.rb
+++ lib/java_web_tomcat_pack.rb
@@ -29,7 +29,7 @@ class JavaWebTomcatPack < JavaPack
     puts "#{global.build_path}"
     puts "****************************"
     
-    SystemUtil.run_with_err_output("mkdir -p #{global.tomcat_dir}/webapps/ROOT && mv #{global.build_path}/ #{global.tomcat_dir}/webapps/ROOT")
+    SystemUtil.run_with_err_output("mkdir -p #{global.tomcat_dir}/webapps/ROOT && mv #{global.build_path}/* #{global.tomcat_dir}/webapps/ROOT")
   end
 
   def move_tomcat_to_root
