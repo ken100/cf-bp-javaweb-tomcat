d0b0d72bad8d1921a068786cc7dc55c5cbdc7493
yang-zhao <zhaoyang_100@hotmail.com>
2014-06-03 11:13:58 +0900

    Update java_web_tomcat_pack.rb

--- lib/java_web_tomcat_pack.rb
+++ lib/java_web_tomcat_pack.rb
@@ -16,10 +16,10 @@ class JavaWebTomcatPack < JavaPack
   def compile
     Fetcher.install_jdk(global)
     Fetcher.install_tomcat(global)
-    #copy_webapp_to_tomcat
+    copy_webapp_to_tomcat
     #move_tomcat_to_root
     #copy_resources
-    setup_profiled
+    #setup_profiled
   end
 
   def copy_webapp_to_tomcat
