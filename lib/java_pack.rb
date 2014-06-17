13d43b0bd348ba6c6d96407b42ab175cedb504f0
yang-zhao <zhaoyang_100@hotmail.com>
2014-06-17 14:31:54 +0900

    Update java_pack.rb

--- lib/java_pack.rb
+++ lib/java_pack.rb
@@ -36,8 +36,8 @@ class JavaPack
   def bash_script
     <<-BASH
 #!/usr/bin/env bash
-export JAVA_HOME="/home/vcap/app/.jdk"
-export PATH="/home/vcap/app/.jdk/bin:$PATH"
+export JAVA_HOME="#{global.build_path}.jdk"
+export PATH="#{global.build_path}.jdk/bin:$PATH"
 export JAVA_OPTS=${JAVA_OPTS:-"#{java_opts.map{ |k, v| "#{k}#{v}" }.join(' ')}"}
 export LANG="${LANG:-en_US.UTF-8}"
 
